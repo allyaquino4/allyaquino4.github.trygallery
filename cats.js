6d29cb60f1615a2de777dacc7a6d4f0f40878fcf
allyaquino4 <c.allysaaquino@gmail.com>
2025-11-27 22:47:22 +0800

    refactor

--- cats.js
+++ cats.js
@@ -1,89 +1,29 @@
 const catsData = [
   {
-    "group": "My Cats",
-    "members": [
-      {
-        "name": "Loonie",
-        "birthdate": "2020-11-22",
-        "description": "Aggressive but sweet big boy",
-        "img": "ASSETS/loonie.jpg"
-      },
-      {
-        "name": "Tyra",
-        "birthdate": "2021-06-09",
-        "description": "The best momma cat ever",
-        "img": "ASSETS/tyra.jpg"
-      },
-      {
-        "name": "Chichi",
-        "birthdate": "2023-12-18",
-        "description": "The bouncy baby girl.",
-        "img": "ASSETS/chichi.jpg"
-      },
-      {
-        "name": "Clea",
-        "birthdate": "2023-12-18",
-        "description": "The pretty and sassy pussycat.",
-        "img": "ASSETS/clea.jpg"
-      },
-      {
-        "name": "Caiden",
-        "birthdate": "2023-12-18",
-        "description": "The brave and loving baby boy",
-        "img": "ASSETS/caiden.jpg"
-      },
-      {
-        "name": "Liam",
-        "birthdate": "2024-05-04",
-        "description": "The ready-to-eat-anything cat",
-        "img": "ASSETS/liam.jpg"
-      },
-      {
-        "name": "Tristan",
-        "birthdate": "2024-05-04",
-        "description": "The sweetest and cuddliest cat.",
-        "img": "ASSETS/tristan.jpg"
-      },
-      {
-        "name": "Zein",
-        "birthdate": "2024-05-04",
-        "description": "The curious little explorer.",
-        "img": "ASSETS/zein.jpg"
-      },
-      {
-        "name": "Theo",
-        "birthdate": "2025-07-26",
-        "description": "The miraculous baby kitten.",
-        "img": "ASSETS/theo.jpg"
-      }
+    group: "My Cats",
+    members: [
+      { name: "Loonie", birthdate: "2020-11-22", description: "Aggressive but sweet big boy", img: "assets/loonie.jpg" },
+      { name: "Tyra", birthdate: "2021-06-09", description: "The best momma cat ever", img: "assets/tyra.jpg" },
+      { name: "Chichi", birthdate: "2023-12-18", description: "The bouncy baby girl.", img: "assets/chichi.jpg" },
+      { name: "Clea", birthdate: "2023-12-18", description: "The pretty and sassy pussycat.", img: "assets/clea.jpg" },
+      { name: "Caiden", birthdate: "2023-12-18", description: "The brave and loving baby boy", img: "assets/caiden.jpg" },
+      { name: "Liam", birthdate: "2024-05-04", description: "The ready-to-eat-anything cat", img: "assets/liam.jpg" },
+      { name: "Tristan", birthdate: "2024-05-04", description: "The sweetest and cuddliest cat.", img: "assets/tristan.jpg" },
+      { name: "Zein", birthdate: "2024-05-04", description: "The curious little explorer.", img: "assets/zein.jpg" },
+      { name: "Theo", birthdate: "2025-07-26", description: "The miraculous baby kitten.", img: "assets/theo.jpg" }
     ]
   },
   {
-    "group": "My Angels",
-    "members": [
-      {
-        "name": "Lucas",
-        "birthdate": "2023-12-18",
-        "description": "The little fighter that guides us from above.",
-        "img": "ASSETS/lucas.jpg"
-      },
-      {
-        "name": "Tyler",
-        "birthdate": "2024-05-04",
-        "description": "The sweet cat angel watching over us.",
-        "img": "ASSETS/tyler.jpg"
-      }
+    group: "My Angels",
+    members: [
+      { name: "Lucas", birthdate: "2023-12-18", description: "The little fighter that guides us from above.", img: "assets/lucas.jpg" },
+      { name: "Tyler", birthdate: "2024-05-04", description: "The sweet cat angel watching over us.", img: "assets/tyler.jpg" }
     ]
   },
   {
-    "group": "My Far-away Baby",
-    "members": [
-      {
-        "name": "Shawn",
-        "birthdate": "2024-05-04",
-        "description": "The always-in-my-heart kitty.",
-        "img": "ASSETS/shawn.jpg"
-      }
+    group: "My Far-away Baby",
+    members: [
+      { name: "Shawn", birthdate: "2024-05-04", description: "The always-in-my-heart kitty.", img: "assets/shawn.jpg" }
     ]
   }
 ];
